5dd827d9c4f1c064542276d0bfc8f749f3387218
Maxwell Okumu <maxmaex69@gmail.com>
2018-09-18 10:25:03 +0300

    intial commit

---
+++ js/myfile.js
@@ -0,0 +1,13 @@
+
+(function($) {
+    "use strict"; // Start of use strict
+    
+    $('#collapseOne').on('show', function () {
+        window.location.href = 'resume/Resume.pdf';
+    })
+
+})(jQuery); // End of use strict
+    $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+        
